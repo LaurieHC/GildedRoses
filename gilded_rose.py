31f6049422b25353039a0f0da98a4a165301586a
LaurieHC <lhumphriescable@gmail.com>
2024-06-23 13:47:52 +0100

    feat(GildedRoses): Implement a class which will be used to easily handle unique functionality of items

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+class ItemAger(object):
+
+    def base_item(item):
+        if item.sell_in > 0:
+            item.quality = max((item.quality + -1), 0)
+        else:
+            item.quality = max((item.quality + -2), 0)
+        item.sell_in += -1
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -10,7 +19,8 @@ class GildedRose(object):
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        # item.quality = item.quality - 1
+                        ItemAger.base_item(item)
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -21,19 +31,19 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            # if item.name != "Sulfuras, Hand of Ragnaros":
+            #     item.sell_in = item.sell_in - 1
+            # if item.sell_in < 0:
+            #     if item.name != "Aged Brie":
+            #         if item.name != "Backstage passes to a TAFKAL80ETC concert":
+            #             if item.quality > 0:
+            #                 if item.name != "Sulfuras, Hand of Ragnaros":
+            #                     item.quality = item.quality - 1
+            #         else:
+            #             item.quality = item.quality - item.quality
+            #     else:
+            #         if item.quality < 50:
+            #             item.quality = item.quality + 1
 
 
 class Item:
